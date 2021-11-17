8c8bec5924f3e6ef528caab1f939a5be119ec9fb
Artem Shram <tobusiness434@gmail.com>
2021-11-18 00:25:33 +0300

    everything works well

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 
 from .models import Group, Post
@@ -8,15 +9,23 @@ gr_p_template = 'posts/group_list.html'
 
 
 def index(request):
-    posts = Post.objects.all()[:10]
-    context = {'posts': posts, 'title': 'Последние обновления на сайте'}
+    posts_list = Post.objects.all()
+    paginator = Paginator(posts_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj, 'title': 'Последние обновления на сайте'}
+    # old:
+    # context = {'posts': posts, 'title': 'Последние обновления на сайте'}
     return render(request, index_template, context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:10]
+    posts_list = group.posts.all()
+    paginator = Paginator(posts_list, 10) 
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {'title': f'Записи сообщества "{group}"',
-               'group': group, 'posts': posts}
+               'group': group, 'page_obj': page_obj}
 
     return render(request, gr_p_template, context)
